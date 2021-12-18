fa805cfd0d5804ec3fa6dd7576693ec40d34064f
Jacob Weber <jacob@jacobweber.com>
2021-12-18 10:34:02 -0800

    consts

--- src/Constants.h
+++ src/Constants.h
@@ -2,8 +2,7 @@
 
 #include <cstddef>
 
-const size_t MAX_STEPS = 4;
+const size_t MAX_STEPS = 16;
 const size_t MAX_VOICES = 4;
 
-const size_t STEP_VALUE = 4; // 1 step per quarter, make float?
 const double PLAY_DELAY_SEC = 0.1; // so cycle starts at first note when you press multiple notes at once
