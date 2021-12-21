82be1cf23767b09bb5cd458e0726704022820d4f
Jacob Weber <jacob@jacobweber.com>
2021-12-20 20:42:54 -0800

    add test utility

---
+++ test/MidiProcesserTester.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <juce_audio_utils/juce_audio_utils.h>
+
+#include "Constants.h"
+#include "ParametersFactory.h"
+#include "MidiProcessor.h"
+#include "TestAudioProcessor.h"
+
+class MidiProcessorTester {
+public:
+    MidiProcessorTester() : MidiProcessorTester(100, 1000) {
+    }
+
+    MidiProcessorTester(int bs, int sr) : bufferSize(bs), sampleRate(sr) {
+        mp.init(sampleRate);
+
+        *(state.stepsParameter) = 3; // index
+        *(state.voicesParameter) = 3; // index
+        *(state.rateParameter) = 2; // index
+        for (size_t i = 0; i < MAX_STEPS; i++) {
+            *(state.stepState[i].lengthParameter) = .5;
+            for (size_t j = 0; j < MAX_VOICES; j++) {
+                *(state.stepState[i].voiceParameters[j]) = false;
+            }
+        }
+
+        posInfo.ppqPosition = 0;
+        posInfo.ppqPositionOfLastBarStart = 0;
+        posInfo.bpm = 120;
+        posInfo.timeSigNumerator = 4;
+        posInfo.timeSigDenominator = 4;
+        posInfo.isPlaying = false;
+        posInfo.isRecording = false;
+
+        midiIn.clear();
+        midiOut.clear();
+
+        lastBlockStartSample = -1;
+    }
+
+    void processBlock() {
+        midiOut.clear();
+        if (lastBlockStartSample == -1) {
+            lastBlockStartSample = 0;
+        } else {
+            lastBlockStartSample += bufferSize;
+        }
+        mp.process(bufferSize, midiIn, midiOut, posInfo, state);
+        midiIn.clear();
+    }
+
+    void processBlocks(int numBlocks) {
+        blocksMidiOutString = "";
+        for (int i = 0; i < numBlocks; i++) {
+            midiOut.clear();
+            if (lastBlockStartSample == -1) {
+                lastBlockStartSample = 0;
+            } else {
+                lastBlockStartSample += bufferSize;
+            }
+            DBG("-- frame " << i << " at " << lastBlockStartSample << " --");
+            mp.process(bufferSize, midiIn, midiOut, posInfo, state);
+            midiIn.clear();
+
+            for (const auto metadata: midiOut) {
+                blocksMidiOutString << (lastBlockStartSample + metadata.samplePosition) << ": "
+                                    << metadata.getMessage().getDescription()
+                                    << "\n";
+            }
+        }
+    }
+
+    juce::String midiOutString() {
+        juce::String out = "";
+        for (const auto metadata: midiOut) {
+            out << (lastBlockStartSample + metadata.samplePosition) << ": " << metadata.getMessage().getDescription()
+                << "\n";
+        }
+        return out;
+    }
+
+public:
+    int bufferSize = 200;
+    int sampleRate = 1000;
+    TestAudioProcessor ap{ParametersFactory::create()};
+    State state{ap.state};
+    juce::AudioPlayHead::CurrentPositionInfo posInfo{};
+    juce::MidiBuffer midiIn{};
+    juce::MidiBuffer midiOut{};
+    MidiProcessor mp{};
+    int lastBlockStartSample;
+    juce::String blocksMidiOutString = "";
+};
--- test/MidiProcessor.cpp
+++ test/MidiProcessor.cpp
@@ -1,50 +1,31 @@
 #include <catch2/catch.hpp>
 
-#include "MidiProcessor.h"
-#include "TestAudioProcessor.h"
-#include "ParametersFactory.h"
+#include "MidiProcesserTester.h"
 
 TEST_CASE("MidiProcessor")
 {
-    const int BLOCK_SIZE = 200;
-    const int SAMPLE_RATE = 1000;
+    MidiProcessorTester tester{};
 
-    juce::MidiBuffer midiIn{};
-    juce::MidiBuffer midiOut{};
-    juce::AudioPlayHead::CurrentPositionInfo posInfo{};
-    TestAudioProcessor ap{ParametersFactory::create()};
-    State state{ap.state};
+    *(tester.state.stepsParameter) = 3; // 3 steps
+    *(tester.state.voicesParameter) = 3; // 4 voices
+    *(tester.state.rateParameter) = 3; // eighths
+    *(tester.state.stepState[0].voiceParameters[3]) = true;
+    *(tester.state.stepState[1].voiceParameters[2]) = true;
+    *(tester.state.stepState[2].voiceParameters[1]) = true;
+    *(tester.state.stepState[3].voiceParameters[0]) = true;
 
-    *(state.stepsParameter) = 3; // index
-    *(state.voicesParameter) = 3; // index
-    *(state.rateParameter) = 2; // index
-    for (size_t i = 0; i < 4; i++) {
-        *(state.stepState[i].lengthParameter) = .5;
-        for (size_t j = 0; j < 4; j++) {
-            *(state.stepState[i].voiceParameters[j]) = (j == i);
-        }
-    }
+    tester.midiIn.addEvent(juce::MidiMessage::noteOn(1, 60, (juce::uint8) 100), 10);
+    tester.midiIn.addEvent(juce::MidiMessage::noteOn(2, 62, (juce::uint8) 101), 9);
+    tester.midiIn.addEvent(juce::MidiMessage::noteOn(3, 64, (juce::uint8) 102), 8);
+    tester.midiIn.addEvent(juce::MidiMessage::noteOn(4, 65, (juce::uint8) 103), 7);
 
-    MidiProcessor mp{};
-    mp.init(SAMPLE_RATE);
-
-    posInfo.ppqPosition = 0;
-    posInfo.ppqPositionOfLastBarStart = 0;
-    posInfo.bpm = 120;
-    posInfo.timeSigNumerator = 4;
-    posInfo.timeSigDenominator = 4;
-    posInfo.isPlaying = false;
-    posInfo.isRecording = false;
-
-    midiIn.clear();
-    midiIn.addEvent(juce::MidiMessage::noteOn(1, 60, (juce::uint8) 100), 10);
-    midiOut.clear();
-    mp.process(BLOCK_SIZE, midiIn, midiOut, posInfo, state);
-
-    juce::String out = "";
-    for (const auto metadata: midiOut) {
-        out << metadata.samplePosition << ": " << metadata.getMessage().getDescription() << "\n";
-    }
-
-    REQUIRE(out == "100: Note on C3 Velocity 100 Channel 1\n");
+    tester.processBlocks(10);
+    REQUIRE(tester.blocksMidiOutString == "100: Note on C3 Velocity 100 Channel 1\n"
+                                          "225: Note off C3 Velocity 0 Channel 1\n"
+                                          "350: Note on D3 Velocity 101 Channel 2\n"
+                                          "475: Note off D3 Velocity 0 Channel 2\n"
+                                          "600: Note on E3 Velocity 102 Channel 3\n"
+                                          "725: Note off E3 Velocity 0 Channel 3\n"
+                                          "850: Note on F3 Velocity 103 Channel 4\n"
+                                          "975: Note off F3 Velocity 0 Channel 4\n");
 }
