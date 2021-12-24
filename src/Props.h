bf7dc3be708c8966306177bb23a51631e9d1e414
Jacob Weber <jacob@jacobweber.com>
2021-12-24 12:20:19 -0800

    constants -> props

--- src/Props.h
+++ src/Props.h
@@ -4,25 +4,25 @@
 #include <cstddef>
 
 namespace props {
-    inline const size_t MAX_STEPS = 16;
-    inline const size_t MAX_VOICES = 8;
-    inline const size_t MAX_OCTAVES = 4;
+    const size_t MAX_STEPS = 16;
+    const size_t MAX_VOICES = 8;
+    const size_t MAX_OCTAVES = 4;
 
-    inline const juce::Colour COLOR_STANDARD = juce::Colours::white;
-    inline const juce::Colour COLOR_OUTLINE = juce::Colours::grey;
-    inline const juce::Colour COLOR_HIGHLIGHT = juce::Colours::red;
+    const juce::Colour COLOR_STANDARD = juce::Colours::white;
+    const juce::Colour COLOR_OUTLINE = juce::Colours::grey;
+    const juce::Colour COLOR_HIGHLIGHT = juce::Colours::red;
 
-    inline const juce::String LABEL_TIE = "Tie";
-    inline const juce::String LABEL_STEPS = "Steps";
-    inline const juce::String LABEL_VOICES = "Steps";
-    inline const juce::String LABEL_RATE = "Step Length";
+    const juce::String LABEL_TIE = "Tie";
+    const juce::String LABEL_STEPS = "Steps";
+    const juce::String LABEL_VOICES = "Steps";
+    const juce::String LABEL_RATE = "Step Length";
 
-    inline const juce::String TOOLTIP_RANDOM = "Randomize settings";
-    inline const juce::String TOOLTIP_VOICES_CLEAR = "Turn off all voices for this step";
-    inline const juce::String TOOLTIP_VOICES_FILL = "Turn on all voices for this step";
-    inline const juce::String TOOLTIP_TIE = "Hold this step instead of playing the next step";
-    inline const juce::String TOOLTIP_POWER_OFF = "Disable this step";
-    inline const juce::String TOOLTIP_POWER_ON = "Enable this step";
+    const juce::String TOOLTIP_RANDOM = "Randomize settings";
+    const juce::String TOOLTIP_VOICES_CLEAR = "Turn off all voices for this step";
+    const juce::String TOOLTIP_VOICES_FILL = "Turn on all voices for this step";
+    const juce::String TOOLTIP_TIE = "Hold this step instead of playing the next step";
+    const juce::String TOOLTIP_POWER_OFF = "Disable this step";
+    const juce::String TOOLTIP_POWER_ON = "Enable this step";
 
-    inline const double PLAY_DELAY_SEC = 0.1; // so cycle starts at first note when you press multiple notes at once
+    const double PLAY_DELAY_SEC = 0.1; // so cycle starts at first note when you press multiple notes at once
 }
