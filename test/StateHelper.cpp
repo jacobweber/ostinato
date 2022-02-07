7a3fc2394e61563eb6281c6c3a7b0d319846a26a
Jacob Weber <jacob@jacobweber.com>
2022-02-06 19:57:06 -0800

    use int for locals

--- test/StateHelper.cpp
+++ test/StateHelper.cpp
@@ -5,24 +5,24 @@
 #include "Stretcher.h"
 
 void StateHelper::setGrid(State &state, juce::String &grid) {
-    auto numSteps = static_cast<stepnum_t>(grid.indexOf("\n"));
-    auto numVoices = static_cast<voicenum_t>(grid.length()) / (numSteps + 1);
-    *(state.stepsParameter) = static_cast<int>(numSteps - 1); // index
-    *(state.voicesParameter) = static_cast<int>(numVoices - 1); // index
-    for (stepnum_t i = 0; i < numSteps; i++) {
-        for (voicenum_t j = 0; j < numVoices; j++) {
-            *(state.stepState[i].voiceParameters[j]) = grid[static_cast<int>((numVoices - 1 - j) * (numSteps + 1) + i)] == '*';
+    int numSteps = grid.indexOf("\n");
+    int numVoices = grid.length() / (numSteps + 1);
+    *(state.stepsParameter) = numSteps - 1; // index
+    *(state.voicesParameter) = numVoices - 1; // index
+    for (size_t i = 0; i < static_cast<size_t>(numSteps); i++) {
+        for (size_t j = 0; j < static_cast<size_t>(numVoices); j++) {
+            *(state.stepState[i].voiceParameters[j]) = grid[(numVoices - 1 - static_cast<int>(j)) * (numSteps + 1) + static_cast<int>(i)] == '*';
         }
         *(state.stepState[i].powerParameter) = true;
     }
 }
 
 juce::String StateHelper::getGrid(State &state) {
-    auto numSteps = static_cast<stepnum_t>(state.stepsParameter->getIndex()) + 1; // index
-    auto numVoices = static_cast<voicenum_t>(state.voicesParameter->getIndex()) + 1; // index
+    int numSteps = state.stepsParameter->getIndex() + 1; // index
+    int numVoices = state.voicesParameter->getIndex() + 1; // index
     juce::String grid = juce::String();
-    for (int j = static_cast<int>(numVoices) - 1; j >= 0; j--) {
-        for (stepnum_t i = 0; i < numSteps; i++) {
+    for (int j = numVoices - 1; j >= 0; j--) {
+        for (size_t i = 0; i < static_cast<size_t>(numSteps); i++) {
             grid += state.stepState[i].voiceParameters[j]->get() ? '*' : '-';
         }
         grid += '\n';
